29e0bd8c13212596c3ddc69ddd07a625c3c1f428
Interdato <Checon91@users.noreply.github.com>
2026-05-18 14:19:35 -0600

    Connect Netlify Blobs in function runtime

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,5 +2,12 @@ const serverless = require("serverless-http");
 const createApp = require("../../src/server/app");
 
 const app = createApp();
+const handler = serverless(app);
 
-module.exports.handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  const { connectLambda } = await import("@netlify/blobs");
+
+  connectLambda(event);
+
+  return handler(event, context);
+};
